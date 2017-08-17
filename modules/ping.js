5c109779f782c6fe6c0081fd517798417c1c542f
Wizzardo Meowy <wizzmurr@outlook.com>
2017-08-17 19:20:18 +0800

    Ping!

--- modules/ping.js
+++ modules/ping.js
@@ -1,22 +1,22 @@
 module.exports.run = async (bot, message, args) => {
     switch (Math.floor(Math.random() * 1000) % 6) {
         case 0:
-            message.channel.send(`Haha! I won by ${bot.ping}ms!`);
+            message.channel.send(`Haha! I won by ${Math.round(bot.ping)}ms!`);
             break;
         case 1:
-            message.channel.send('Much better than "Hello World!" | ' + bot.ping + 'ms`'); 
+            message.channel.send('Much better than "Hello World!" | ' + Math.round(bot.ping) + 'ms`'); 
             break;
         case 2:
-        message.channel.send("Don't expect me to reply with `pong` | " + bot.ping + "ms");
+            message.channel.send("Don't expect me to reply with `pong` | " + Math.round(bot.ping) + "ms");
             break;
         case 3:
-            message.channel.send(`Tennis! | ${bot.ping}ms`);
+            message.channel.send(`Tennis! | ${Math.round(bot.ping)}ms`);
             break;
         case 4:
-            message.channel.send(`Badminton! | ${bot.ping}ms`);
+            message.channel.send(`Badminton! | ${Math.round(bot.ping)}ms`);
             break;
         case 5:
-            message.channel.send(`Table Tennis! | ${bot.ping}ms`);
+            message.channel.send(`Table Tennis! | ${Math.round(bot.ping)}ms`);
             break;
     }
 }
